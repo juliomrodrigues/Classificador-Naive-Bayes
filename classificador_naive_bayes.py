f2a92e7025b5dd4f228873721a242b25d6b0a584
Júlio Rodrigues <76015049+juliomrodrigues@users.noreply.github.com>
2021-02-04 15:51:21 -0300

    Código Fonte
    Atualizando Código fonte(sem matriz)

--- classificador_naive_bayes.py
+++ classificador_naive_bayes.py
@@ -6,22 +6,23 @@ Base de Dados: Dua, D. and Graff, C. (2019). UCI Machine Learning Repository
 [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School 
 of Information and Computer Science.
 """
-
 import pandas
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
  
 base = pandas.read_csv('census.csv')
  
 atributos = base.iloc[:, 0:14].values
 classe = base.iloc[:, 14].values
 
-# Transformando variaveis categóricas(Strings) em números
+# LABEL_ENCODER
 label_encoder= LabelEncoder()
+classe = label_encoder.fit_transform(classe)
 atributos[:, 1] = label_encoder.fit_transform(atributos[:, 1])
 atributos[:, 3] = label_encoder.fit_transform(atributos[:, 3])
 atributos[:, 5] = label_encoder.fit_transform(atributos[:, 5])
@@ -31,20 +32,26 @@ atributos[:, 8] = label_encoder.fit_transform(atributos[:, 8])
 atributos[:, 9] = label_encoder.fit_transform(atributos[:, 9])
 atributos[:, 13] = label_encoder.fit_transform(atributos[:, 13])
 
-classe = label_encoder.fit_transform(classe)
+# ONE_HOT_ENCODER
+one_hot_encoder = OneHotEncoder()
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
+atributos = column_tranformer.fit_transform(atributos).toarray()
 
-#Aplicando escalonamento nos valores
+# STANDARD_SCALER
 scaler = StandardScaler()
 atributos = scaler.fit_transform(atributos)
-previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(atributos, classe, test_size=0.15, random_state=0)
 
-# Gerando Tabela de Classificação
-classificador = GaussianNB()
-classificador.fit(previsores_treinamento, classe_treinamento)
-previsoes = classificador.predict(previsores_teste)
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+resultados = []
+for indice_treinamento, indice_teste in skfold.split(atributos,
+                                                    np.zeros(shape=(atributos.shape[0], 1))):
+   classificador = GaussianNB()
+   classificador.fit(atributos[indice_treinamento], classe[indice_treinamento])     
+   previsoes = classificador.predict(atributos[indice_teste])
+   precisao = accuracy_score(classe[indice_teste], previsoes)
+   resultados.append(precisao)
+   
+resultados = np.asarray(resultados)
 
-# Calculando a precisão e gerando matriz de confusão
-taxa_precisao = accuracy_score(classe_teste, previsoes)
-matriz = confusion_matrix(classe_teste, previsoes)
-
-print(f'A taxa de precisão alcançada foi de {round(taxa_precisao*100)}%')
+print(f'Média = {resultados.mean()}')
+print(f'Desvio Padrão = {resultados.std()}')
